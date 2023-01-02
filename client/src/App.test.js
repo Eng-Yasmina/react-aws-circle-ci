c4a2808147af1276cc92c7bdeefa90b381047f98
Eng-Yasmina <aliyasoo70@gmail.com>
2023-01-02 04:20:27 +0200

    fix unit testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('correctly renders override header level', () => {
+  const { getByRole } = render(<h3/>)
+
+  expect(getByRole('heading', { level: 3 })).toBeInTheDocument()
 });
